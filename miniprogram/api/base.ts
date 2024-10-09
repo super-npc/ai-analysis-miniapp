f328c094402ea3b3a7ed270affd7c682e444caa4
super-npc <super_npc@sina.cn>
2024-10-09 18:05:34 +0800

    卡片

--- miniprogram/api/base.ts
+++ miniprogram/api/base.ts
@@ -11,7 +11,8 @@ const envVersion = accountInfo.miniProgram.envVersion || 'release'
   */
 const GDEnvs = {
   develop: {
-    host: 'http://192.168.31.141:8080',
+    host: 'http://127.0.0.1:9090',
+    // host: 'http://192.168.31.141:8080',
     imgHost: 'http://192.168.0.2:20087'
   },
   trial: {
---
+++ miniprogram/pages/demo/index.ts
@@ -0,0 +1,24 @@
+// pages/demo/index.ts
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    rowCol: [{ size: '327rpx', borderRadius: '24rpx' }, 1, { width: '61%' }],
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
