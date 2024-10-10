1f77dce84715953b0ad62e4bb3f1b26328438903
super-npc <super_npc@sina.cn>
2024-10-10 17:28:12 +0800

    grid

--- miniprogram/pages/demo/index.ts
+++ miniprogram/pages/demo/index.ts
@@ -1,24 +1,47 @@
-// pages/demo/index.ts
 Component({
-
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
-
-  /**
-   * 组件的初始数据
-   */
   data: {
-    rowCol: [{ size: '327rpx', borderRadius: '24rpx' }, 1, { width: '61%' }],
+    menuItems: [
+      {
+        id: 1,
+        name: '白米饭',
+        description: '白米饭',
+        price: '1.00',
+        status: '已售罄',
+        image: '/images/white-rice.jpg'
+      },
+      {
+        id: 2,
+        name: '杂粮米',
+        description: '杂粮米',
+        price: '1.30',
+        status: '已售罄',
+        image: '/images/mixed-grain-rice.jpg'
+      },
+      {
+        id: 3,
+        name: '传统酱油烧肉',
+        description: '五花肉焖油煎至金黄，葱、蒜、姜',
+        price: '7.00',
+        status: '已售罄',
+        image: '/images/soy-sauce-pork.jpg'
+      },
+      {
+        id: 4,
+        name: '传统酱油烧肉（半份）',
+        description: '五花肉焖油煎至金黄，葱、蒜、姜',
+        price: '3.50',
+        status: '已售罄',
+        image: '/images/soy-sauce-pork-half.jpg'
+      }
+    ]
   },
 
-  /**
-   * 组件的方法列表
-   */
   methods: {
-
+    onItemTap(event: any) {
+      const item = event.currentTarget.dataset.item;
+      wx.navigateTo({
+        url: `/pages/ai_analysis/index?item=${JSON.stringify(item)}`,
+      });
+    }
   }
 })
