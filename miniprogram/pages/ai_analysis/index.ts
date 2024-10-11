2365f86acb32dc6de3d449a3d187b7a376da1ba4
super-npc <super_npc@sina.cn>
2024-10-11 08:29:23 +0800

    滚动提示

--- miniprogram/pages/ai_analysis/index.ts
+++ miniprogram/pages/ai_analysis/index.ts
@@ -19,7 +19,13 @@ Component({
     showCamera: false,
     src: '',
     analyseResults: [] as AnalyseResult[],
-    bigModelResp: {} as BigModelResp // 添加 BigModelResp 对象
+    bigModelResp: {} as BigModelResp, // 添加 BigModelResp 对象
+    visible: true,
+    marquee2: {
+      speed: 60,
+      loop: -1,
+      delay: 0,
+    },
   },
 
   /**
