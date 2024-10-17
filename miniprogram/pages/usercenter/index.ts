9b34f93bb7de84f0cd817a3ac05d65b3630c384d
super-npc <super_npc@sina.cn>
2024-10-17 12:44:54 +0800

    昵称/头像

--- miniprogram/pages/usercenter/index.ts
+++ miniprogram/pages/usercenter/index.ts
@@ -2,6 +2,8 @@
 import { WxMaSessionResp } from "../../api/controller/MiniAppBaseController";
 import LoginResCache from "../../cache/LoginResCache";
 
+const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+
 Component({
 
   /**
@@ -15,7 +17,12 @@ Component({
    * 组件的初始数据
    */
   data: {
-    username: '请先登录'
+    userInfo: {
+      avatarUrl: defaultAvatarUrl,
+      nickName: '',
+    },
+    canIUseGetUserProfile: wx.canIUse('getUserProfile'),
+    canIUseNicknameComp: wx.canIUse('input.type.nickname')
   },
 
   /**
@@ -31,9 +38,25 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    onChooseAvatar(e: any) {
+      const { avatarUrl } = e.detail
+      const { nickName } = this.data.userInfo
+      this.setData({
+        "userInfo.avatarUrl": avatarUrl,
+        hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
+      })
+    },
+    onInputChange(e: any) {
+      const nickName = e.detail.value
+      const { avatarUrl } = this.data.userInfo
+      this.setData({
+        "userInfo.nickName": nickName,
+        hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
+      })
+    },
     loadUserInfo() {
-      LoginResCache.getStorage().then((res) =>{
-        const wxMaSessionResp  = res as WxMaSessionResp;
+      LoginResCache.getStorage().then((res) => {
+        const wxMaSessionResp = res as WxMaSessionResp;
         this.setData({
           username: wxMaSessionResp.openid || '未知用户'
         });
@@ -44,6 +67,19 @@ Component({
       const index = event.currentTarget.dataset.index;
       // 根据 index 处理点击事件
       console.log(`点击了设置 ${index + 1}`);
+    },
+    getUserProfile() {
+      // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+      wx.getUserProfile({
+        desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+        success: (res) => {
+          console.log(res)
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
     }
   }
 })
